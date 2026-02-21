9a565f82203f5868831d0513471702beffa905b1
elchechulin <136129283+elchechulin@users.noreply.github.com>
2026-02-21 03:18:35 +0100

    Update health.js

--- api/health.js
+++ api/health.js
@@ -22,6 +22,14 @@ function buffer(readable) {
 
 export default async function handler(req, res) {
 
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+if (req.method === "OPTIONS") {
+  return res.status(200).end();
+}
+
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Stripe-Signature");
